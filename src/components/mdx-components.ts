5546acf0fc26d17cb16c8e2acd6f3e377073daeb
krokerdile <crazykroker@gmail.com>
2025-06-07 17:13:04 +0900

    chore: MDX 내에서 사용할 컴포넌트 레지스트리 초기 구성
    • InfiniteScrollBlogPage 컴포넌트를 allMdxComponents에 등록
    • MDX 페이지에서 JSX 컴포넌트를 쉽게 사용할 수 있도록 통합 관리
    • 향후 컴포넌트 추가 시 해당 객체만 유지보수하면 되도록 설계

---
+++ src/components/mdx-components.ts
@@ -0,0 +1,7 @@
+import InfiniteScrollBlogPage from '@/components/InfiniteScrollBlogPage'
+// 필요한 컴포넌트 계속 추가
+
+export const allMdxComponents = {
+  InfiniteScrollBlogPage,
+  // 앞으로 더 추가되어도 여기만 유지
+}
