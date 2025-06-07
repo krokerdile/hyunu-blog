3dfa33ffb117803d9f05ed87bfa900bc1fe31cc0
krokerdile <crazykroker@gmail.com>
2025-06-07 16:49:54 +0900

    feat: mdx-layout 컴포넌트 스타일 및 구조 추가

---
+++ src/components/mdx-layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+export default function MdxLayout({ children }: { children: ReactNode }) {
+  // 여기에서 모든 공유 레이아웃 또는 스타일을 생성하세요
+  return (
+    <div
+      className="prose prose-headings:mt-8 prose-headings:font-semibold prose
+ 
+-headings:text-black prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-headings:text-white"
+    >
+      {children}
+    </div>
+  )
+}
